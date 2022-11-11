facf2f04a594a5d5993a5694d77c721e1605f982
Krzysztof Brzozowski <krzysztofbrzozowski7@gmail.com>
2022-11-11 17:46:15 +0100

    changed errorBoundary fallback

--- src/components/ErrorBoundaryFallback/ErrorBoundaryFallback.tsx
+++ src/components/ErrorBoundaryFallback/ErrorBoundaryFallback.tsx
@@ -1,3 +1,15 @@
-export default function ErrorBoundaryFallback() {
-  return <div>Ups, something went wrong :/</div>;
-}
+import { FallbackProps } from "react-error-boundary";
+
+const ErrorBoundaryFallback = ({
+  error,
+  resetErrorBoundary,
+}:FallbackProps) => {
+  return (
+    <div>
+      <h1>An error occurred: {error.message}</h1>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+};
+
+export default ErrorBoundaryFallback;
